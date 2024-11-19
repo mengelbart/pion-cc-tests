61d532e4f16de4978002b98c455550dd7ab5c510
Mathis Engelbart <mathis.engelbart@gmail.com>
2024-11-19 15:52:17 +0100

    Add submodules

---
+++ generate_configs.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python
+
+import argparse
+from dataclasses import asdict
+import itertools
+
+import yaml
+
+from cctestbed2.configuration import ApplicationConfig, EnvVariable, NetworkConfig, Testcase
+
+
+TC_SETTINGS = [
+    {
+        'name': 'static',
+        'phases': [
+            {
+                'tc': True,
+                'bandwidth': '5mbit',
+                'latency': '50ms',
+                'delay': 100000,
+                'duration': 30,
+            },
+        ],
+    },
+    {
+        'name': 'variable',
+        'phases': [
+            {
+                'tc': True,
+                'bandwidth': '5mbit',
+                'latency': '50ms',
+                'delay': 100000,
+                'duration': 10,
+            },
+            {
+                'tc': True,
+                'bandwidth': '1mbit',
+                'latency': '50ms',
+                'delay': 100000,
+                'duration': 10,
+            },
+            {
+                'tc': True,
+                'bandwidth': '5mbit',
+                'latency': '50ms',
+                'delay': 100000,
+                'duration': 10,
+            },
+        ],
+    },
+]
+
+
+APP_SETTINGS = [
+    {
+        'name': 'gcc',
+        'apps': [
+            {
+                'name': 'receiver',
+                'namespace': 'ns1',
+                'start_time': 0,
+                'duration': 30,
+                'environment': [],
+                'binary': '../../pioncc/pioncc',
+                'arguments': [
+                    '-receiver', '10.1.0.10:8080',
+                    '-sender', '10.3.0.20:8080',
+                ],
+            },
+            {
+                'name': 'sender',
+                'namespace': 'ns4',
+                'start_time': 0,
+                'duration': 30,
+                'environment': [],
+                'binary': '../../pioncc/pioncc',
+                'arguments': [
+                    '-send',
+                    '-receiver', '10.1.0.10:8080',
+                    '-sender', '10.3.0.20:8080',
+                ],
+            },
+        ],
+    },
+]
+
+
+def generate_retralid_configs(directory):
+    combinations = itertools.product(
+        TC_SETTINGS,
+        APP_SETTINGS,
+    )
+    for combination in combinations:
+        tc = combination[0]
+        app_settings = combination[1]
+        name = f'{tc["name"]}_{app_settings["name"]}'
+
+        apps = [ApplicationConfig(
+            name=app['name'],
+            namespace=app['namespace'],
+            start_time=app['start_time'],
+            duration=app['duration'],
+            environment=app['environment'],
+            binary=app['binary'],
+            arguments=app['arguments'],
+        ) for app in app_settings['apps']]
+
+        nc = [NetworkConfig(
+            traffic_control=p['tc'],
+            duration=p['duration'],
+            bandwidth=p['bandwidth'],
+            latency=p['latency'],
+            delay=p['delay']
+        ) for p in tc['phases']]
+
+        duration = sum([p.duration for p in nc])
+
+        tc = Testcase(
+            name=name,
+            network=nc,
+            applications=apps,
+            duration=30,
+        )
+        with open(f'{directory}/{name}.yaml', 'w') as file:
+            file.write(yaml.dump(asdict(tc)))
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '-o', '--output', help='output directory', required=True)
+    args = parser.parse_args()
+    generate_retralid_configs(args.output)
+
+
+if __name__ == "__main__":
+    main()
